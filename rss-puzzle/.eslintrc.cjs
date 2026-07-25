9bae2c5ce892fa01aaafa029b1c3862f4ff50fb6
Khabib Gadzhimagomedov <khabib.x.gadzhimagomedov@gmail.com>
2026-07-26 02:56:12 +0300

    chore(eslint): update TypeScript ESLint config and lint scripts

--- rss-puzzle/.eslintrc.cjs
+++ rss-puzzle/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: ['./tsconfig.json'],
   },
 
   plugins: ['@typescript-eslint', 'import'],
